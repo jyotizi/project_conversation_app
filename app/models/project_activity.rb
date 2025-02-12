cb2d14c35904db856b5ddf9db80db1c19080c601
jyotizi <jyotipatelc0523@gmail.com>
2025-02-12 16:36:29 +0530

    Added model and devise configuration

---
+++ app/models/project_activity.rb
@@ -0,0 +1,4 @@
+class ProjectActivity < ApplicationRecord
+  belongs_to :project
+  belongs_to :user
+end
---
+++ db/migrate/20250212102139_create_project_activities.rb
@@ -0,0 +1,14 @@
+class CreateProjectActivities < ActiveRecord::Migration[7.1]
+  def change
+    create_table :project_activities do |t|
+      t.string :activity_type
+      t.text :comment
+      t.string :old_status
+      t.string :new_status
+      t.references :project, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
